c9116b81233b1ddf34124d9aa3e3f7228b24954a
nk6588 <nk6588@srmist.edu.in>
2025-11-29 15:42:30 +0000

    Update Backend-project

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/onlinebookstore";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(mongoURI);
         console.log("Successfully Connected to MongoDB Database");
     } catch (error) {
         console.log("Failed to connect to the DB", error);
